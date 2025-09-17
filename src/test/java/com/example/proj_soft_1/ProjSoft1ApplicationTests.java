5ade44ecdf111c65f9efb4565ce8ddbdcdeaf443
HenryIdesis <henryi@al.insper.edu.br>
2025-09-17 10:44:10 -0300

    configurando

---
+++ src/test/java/com/example/proj_soft_1/ProjSoft1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proj_soft_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjSoft1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
